ebb9d00522255d8c5456b86e347cff1edd0d434a
Andrew Dong <andrew@aglive.com>
2020-07-31 13:30:15 +1000

    fix test case

--- bin/test.sh
+++ bin/test.sh
@@ -5,7 +5,9 @@ set -e
 ganache-cli --gasLimit 99999999999 2> /dev/null 1> /dev/null &
 sleep 5 # to make sure ganache-cli is up and running before compiling
 rm -rf build
+cp secrets.sample.json secrets.json
 truffle compile
 truffle migrate --reset --network development
 truffle test
+rm secrets.json
 kill -9 $(lsof -t -i:8545)
